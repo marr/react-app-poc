46534b688d829cccb047338a5fffbcf7c69591c1
Dave Marr <dave.marr@gmail.com>
2017-03-07 15:35:01 -0800

    Demonstrating that env vars don't work with react-scripts@0.9.4

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const { env } = process;
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+          <pre>{JSON.stringify(env, '  ', null)}</pre>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
